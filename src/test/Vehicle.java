c204ce4e55b4a00f0def580747421e4e81726bcb
sohal <sohalchunarkar8@gmail.com>
2025-12-16 06:24:29 +0530

    added these  files

---
+++ src/test/Vehicle.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Vehicle {
+    public static void main(String[] args) {
+        air  v1 = new air();
+        v1.fule();
+    }
+}
---
+++ src/test/air.java
@@ -0,0 +1,14 @@
+package test;
+
+public class air {
+        
+    public void fule(){
+        System.out.println("this particular thing runs on air");
+    }
+    public void tyre(){
+        System.out.println("this particular thing has no tyre");
+    }
+    public void engine(){
+        System.out.println("this particular thing has air engine");
+    }
+}
