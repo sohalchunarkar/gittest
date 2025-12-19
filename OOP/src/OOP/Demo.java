44ee988eae78eaccab356c7a9e8afb75081ed83d
sohal <sohalchunarkar8@gmail.com>
2025-12-19 21:46:29 +0530

    s

--- OOP/src/OOP/Demo.java
+++ OOP/src/OOP/Demo.java
@@ -3,8 +3,10 @@ package OOP;
 public class Demo {
   public static void main(String[] args) {
     Teacher t1 = new Teacher();
-    t1.demo1();
-    // t1.demo2();
+    t1.date = 23;
+    // t1.demo1();
+    // t1.demo2()
+    // ;
     Teacher.demo1();
   }
 }
