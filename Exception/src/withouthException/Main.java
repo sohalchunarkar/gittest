37bfa2884fcbe2c655e1cbfe10a220ef7e57531a
sohal <sohalchunarkar8@gmail.com>
2025-12-21 10:41:28 +0530

    Added the Package Without Exception

---
+++ Exception/src/withouthException/Main.java
@@ -0,0 +1,12 @@
+package withouthException;
+
+public class Main extends Object {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+
+        int res = a / b;
+        System.out.println(res);
+        //This is the Code which is Without Exception Handling
+    }
+}
